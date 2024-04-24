08f79e324b0172e61ce5eeff20b3cd8998fa3796
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-04-24 10:39:03 +0700

    feat: fix time

--- src/utils/formatUtils.ts
+++ src/utils/formatUtils.ts
@@ -3,7 +3,7 @@ export const formatUtils = {
     const date = new Date(timestamp);
 
     // Convert to Pacific Time (PT)
-    date.setHours(date.getHours() + 7);
+    date.setHours(date.getHours());
 
     // Get hours and minutes in 24-hour format
     const hours = String(date.getHours()).padStart(2, "0");
@@ -13,12 +13,14 @@ export const formatUtils = {
     const formattedTime = `${hours}.${minutes}`;
     return formattedTime;
   },
-  hashPrivateChatName (username1 : string,username2 : string) : string{
-    if(username2 < username1) [username1,username2] = [username2,username1]
+  hashPrivateChatName(username1: string, username2: string): string {
+    if (username2 < username1) [username1, username2] = [username2, username1];
     return `${username1}-${username2}`;
   },
-  getTargetUsername (hashUsername : string , myUsername : string) : string {
+  getTargetUsername(hashUsername: string, myUsername: string): string {
     const splitUsername = hashUsername.split("-");
-    return splitUsername[0] === myUsername ? splitUsername[1] : splitUsername[0];
-  }
+    return splitUsername[0] === myUsername
+      ? splitUsername[1]
+      : splitUsername[0];
+  },
 };
