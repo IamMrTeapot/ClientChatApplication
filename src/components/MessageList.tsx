a43f6f98e84de4ced49797f6b4f55beaabcf5595
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-04-24 09:16:43 +0700

    feat: auto scroll down

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Message, { IMessage } from "./Message";
 
 export default function MessageList({
@@ -5,10 +6,19 @@ export default function MessageList({
 }: {
   messageList: IMessage[];
 }) {
+  const messageListRef = useRef<HTMLDivElement>(null);
+
+  //2️⃣ watch for when new items are added
+  useEffect(() => {
+    //3️⃣ bring the last item into view
+    messageListRef.current?.lastElementChild?.scrollIntoView();
+  }, [messageList]);
+
   return (
     <div
       className="pt-6 pb-3 overflow-auto flex flex-col gap-2 
 scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-gray-300 scrollbar-track-[#3B3B3B]"
+      ref={messageListRef}
     >
       {messageList.map((message, index) => (
         <Message key={index} {...message} />
